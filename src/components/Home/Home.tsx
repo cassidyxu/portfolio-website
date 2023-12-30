e2e1912615e948ce0da3bcbc77c85d73f7ea3f03
cassidyxu <cassidyh.xu1@gmail.com>
2023-12-30 00:03:18 -0500

    added scroll indicator to home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,10 @@
 import './Home.scss'
 import { useState } from 'react'
 import { motion } from "framer-motion"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
 import penguin from '../../assets/images/penguin.gif'
+
 
 const Home = () => {
 
@@ -9,8 +12,8 @@ const Home = () => {
 
     return (
         <div>
-            <div className="home-page py-16 h-screen flex items-center justify-center">
-                <div className="m-16 flex flex-col md:flex-row items-center justify-center">
+            <div className="home-page py-16 h-screen flex flex-col items-center justify-center">
+                <div className="m-16 flex flex-col md:flex-row items-center justify-center flex-1">
                     <motion.img
                         className="h-200 md:h-300"
                         animate={{ rotate: rotate ? 360 : 0 }}
@@ -26,19 +29,40 @@ const Home = () => {
                             className="intro"
                             initial={{ opacity: 0 }}
                             animate={{ opacity: 1 }}
-                            transition={{ duration: 3, delay: 0.5, ease: "easeIn" }}
+                            transition={{ duration: 2, delay: 0.5, ease: "easeIn" }}
                         >
                             Hi, I'm Cassidy
                         </motion.h1>
                         <motion.h2
                             initial={{ opacity: 0 }}
                             animate={{ opacity: 1 }}
-                            transition={{ duration: 1, delay: 3, ease: "easeIn" }}
+                            transition={{ duration: 1, delay: 2, ease: "easeIn" }}
                         >
                             Click the penguin to see a trick :)
                         </motion.h2>
                     </div>
                 </div>
+                <motion.div
+                    // className="chevron-icon pb-6 md:pb-0"
+                    // initial={{ opacity: 0 }}
+                    // animate={{ opacity: 1 }}
+                    // transition={{ duration: 1, delay: 4, ease: "easeIn" }}
+                    className="mt-auto flex justify-center pb-6 md:pb-0"
+                    initial={{ opacity: 0, y: 0 }}
+                    animate={{
+                        opacity: 1,
+                        y: [0, -10, 0], // Change the values to adjust the bounce
+                    }}
+                    transition={{
+                        duration: 2, // Adjust the duration of each bounce
+                        repeat: Infinity,
+                        repeatType: 'reverse', // Loop the animation
+                        ease: 'easeInOut',
+                        delay: 4, // Delay for 5 seconds before the first bounce
+                    }}
+                >
+                    <FontAwesomeIcon icon={faChevronDown}></FontAwesomeIcon>
+                </motion.div>
             </div>
 
             {/* <div id="aboutPage">
