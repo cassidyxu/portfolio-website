b0357ca6be322df50188c6bbb5c5c2501a71b66f
cassidyxu <cassidyh.xu1@gmail.com>
2023-12-29 23:14:20 -0500

    embedded travel map on travel page

--- src/pages/Travel/Travel.tsx
+++ src/pages/Travel/Travel.tsx
@@ -1,9 +1,15 @@
+import Navbar from '../../components/Navbar/Navbar'
 import './Travel.scss'
 
 const Travel = () => {
     return (
-        <div>
-            <h1>hi from travel</h1>
+        <div className="travel-page w-full h-screen flex flex-col justify-center items-center p-10 md:p-16">
+            <Navbar />
+            <h1 className="pt-10 pb-6">See where I've been!</h1>
+            <iframe
+                src="https://www.google.com/maps/d/embed?mid=1Exu3pJ-4B8rmlmXAgPs3i2lniJV9F24&ehbc=2E312F"
+                className="w-full max-w-1020 h-full"
+            ></iframe>
         </div>
     )
 }
